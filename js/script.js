360d23b274a071f19208b971b9bbd3dc80dac485
megan_ducote <meganducote7@gmail.com>
2026-01-02 16:16:42 -0700

    updated styling and implemented button

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var tripInfoButton = document.querySelector(".trip-info");
+var dailyBudget = document.querySelector(".daily-budget"); 
+
+var tripInfo = function() {
+    var totalBudget = Number(prompt("What is your total budget for the trip?"));
+    var accomodation = Number(prompt("What are your accomodation costs?"));
+    var numDays = Number(prompt("How many days will you be traveling?"));
+
+    calculateDailyBudget(totalBudget, accomodation, numDays);
+};
+
+var calculateDailyBudget = function(totalBudget, accomodation, numDays) {
+    var daily = ((totalBudget - accomodation) / numDays).toFixed(2);
+    dailyBudget.innerText = `You can spend $${daily} a day on food and fun.`;
+};
+
+tripInfoButton.addEventListener("click", tripInfo); 
